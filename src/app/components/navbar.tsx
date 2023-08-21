9743e1a722a6c54d6a8b36c82557288adf4c8868
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-08-20 22:09:52 -0400

    nav all functional

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,22 +1,51 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 export default function Navbar(){
 
     const [selectedTab, setSelectedTab] = useState(0);
-    const [barWidth, setBarWidth] = useState( )
+    const [barWidth, setBarWidth] = useState<number>(0)
+    const [barTranslate, setBarTranslate] = useState<number>(0)
 
-    const ProcessAnimationBar = (toTab:'1'|'2'|'3'|'4') => {
-        if(document){
-            for(let i = barWidth; i <= document.getElementById(toTab)!.offsetWidth; i++){
-            ``
-            }
+    useEffect(function(){
+        setBarWidth(document.getElementById('0')?.clientWidth!)
+    }, [])
+
+    const ProcessAnimationBar = (toTab:'0'|'1'|'2'|'3') => {
+        let before = 0
+        let between = 0
+
+
+        for(let i = 0; i <= Number(toTab) -1; i++){before += document.getElementById(i.toString())?.offsetWidth!
+            console.log(before)}
+
+        
+
+        if(selectedTab < Number(toTab)){
+        for(let i = selectedTab; i <= Number(toTab); i++){between += document.getElementById(i.toString())?.offsetWidth!
+        console.log(between)}
+        }else{
+        for(let i = selectedTab; i >= Number(toTab); i--){between += document.getElementById(i.toString())?.offsetWidth!
+        console.log(between)}
         }
 
+        
 
+        setBarWidth(between)
+        if(selectedTab > Number(toTab))setBarTranslate(before)
+
+        
+
+
+        setTimeout(() => {
+        setBarWidth(document.getElementById(toTab)?.offsetWidth!)
+        if(selectedTab < Number(toTab))setBarTranslate(before)
+        }, 700);
+
+        setSelectedTab(Number(toTab))
     }
 
     
@@ -28,12 +57,13 @@ export default function Navbar(){
         <nav>
             <div className="flex flex-col font-mono text-[16px] w-min">
                 <ul className="flex flex-row items-baseline">
-                    <li className="pr-10" id='0'>home</li>
-                    <li className="pr-10" id='1'>blog</li>
-                    <li className="pr-10" id='2'>guestbook</li>
-                    <li className="pr-10" id='3'>contact</li>
+                    <li className="pr-10" id='0' onClick={()=>ProcessAnimationBar("0")} >home</li>
+                    <li className="pr-10" id='1' onClick={()=>ProcessAnimationBar("1")}>blog</li>
+                    <li className="pr-10" id='2' onClick={()=>ProcessAnimationBar("2")}>guestbook</li>
+                    <li className="pr-10" id='3' onClick={()=>ProcessAnimationBar("3")}>contact</li>
                 </ul>
-                <div className="w-full bg-gray-200 h-[1px] mt-2"></div>
+                <div className="w-full bg-gray-200 h-[1px]   mt-2"></div>
+                <div style={{width: barWidth + 'px', transform: `translate(${barTranslate}px, -2px)`}} className={`bg-black transition-all duration-700  h-[3px]`}></div>
 
             </div>
         </nav>
