611f5f6b8978e2ff79b277943fa3e442479193ba
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-08-21 18:54:42 -0400

    ayyy its not shitty now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import { useCallback, useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import {
-    Link as Linc,
-    Button,
-    Element,
-    Events,
-    animateScroll as Ascroll,
-    scrollSpy,
-    scroller,
-  } from "react-scroll";
+  Link as Linc,
+  Button,
+  Element,
+  Events,
+  animateScroll as Ascroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
 
 import Link from "next/link";
 
@@ -37,12 +37,10 @@ export default function Page() {
 
   const ProcessAnimationBar = useCallback(
     (toTab: string) => {
-        if(toTab != selectedTab.toString()){
-            
-        
+      if (toTab != selectedTab.toString()) {
         let before = 0;
         let between = 0;
-  
+
         for (let i = 0; i <= Number(toTab) - 1; i++) {
           before += document.getElementById(i.toString())?.offsetWidth!;
           before += 40;
@@ -59,57 +57,53 @@ export default function Page() {
           }
         }
         between -= 44;
-  
+
         setBarWidth(between);
         if (selectedTab > Number(toTab)) setBarTranslate(before + 2);
-  
+
         setTimeout(() => {
           setBarWidth(document.getElementById(toTab)?.offsetWidth! + 4);
           if (true) setBarTranslate(before - 2);
         }, 700);
-  
+
         setSelectedTab(Number(toTab));
-    }
+      }
     },
     [selectedTab],
   );
 
-  useEffect(
-    function () {
+  //   useEffect(
+  //     function () {
 
-        if(!scrolling){//unused, always true
-            setTimeout(function(){
-                if (elements[4] < 500) {
-                    ProcessAnimationBar('4')
-                    return;
-                  } else if (elements[3] < 500) {
-                    ProcessAnimationBar('3')
-                    return;
-                  } else if (elements[2] < 500) {
-                    ProcessAnimationBar('2')
-                    return;
-                } else if (elements[1] < 500) {
-                    ProcessAnimationBar('1')
-                    return;
-                  } else {
-                    ProcessAnimationBar('0')
-                    return;
-                  }
-            },1400)
+  //         if(!scrolling){//unused, always true
+  //             setTimeout(function(){
+  //                 if (elements[4] < 500) {
+  //                     ProcessAnimationBar('4')
+  //                     return;
+  //                   } else if (elements[3] < 500) {
+  //                     ProcessAnimationBar('3')
+  //                     return;
+  //                   } else if (elements[2] < 500) {
+  //                     ProcessAnimationBar('2')
+  //                     return;
+  //                 } else if (elements[1] < 500) {
+  //                     ProcessAnimationBar('1')
+  //                     return;
+  //                   } else {
+  //                     ProcessAnimationBar('0')
+  //                     return;
+  //                   }
+  //             },1400)
 
-      //console.log(scroll)
-      //console.log(projects);
+  //       //console.log(scroll)
+  //       //console.log(projects);
 
-    }
-    },
-    [ProcessAnimationBar, elements, scroll, scrolling],
-  );
+  //     }
+  //     },
+  //     [ProcessAnimationBar, elements, scroll, scrolling],
+  //   );
 
-
-
-  const offset=-120
-
-
+  const offset = -120;
 
   return (
     <main className="min-h-screen bg-white">
@@ -119,69 +113,74 @@ export default function Page() {
           <div className="flex flex-row w-full  items-baseline justify-between overflow-x-hidden ">
             <div className="flex flex-col font-mono text-[16px] w-full bg-white">
               <div className="flex flex-row items-baseline">
-                <Linc 
-        activeClass="active"
-      to="home"
-      spy={true}
-      smooth={true}
-      hashSpy={true}
-      offset={offset}
-      duration={1000}
-
-      isDynamic={true}
-      ignoreCancelEvents={true}
-      spyThrottle={500}
-      className="pr-10" onClick={() => ProcessAnimationBar("0")}
-      onSelect={()=>ProcessAnimationBar('0')}>
+                <Linc
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  hashSpy={true}
+                  offset={offset}
+                  duration={1000}
+                  isDynamic={true}
+                  ignoreCancelEvents={true}
+                  spyThrottle={500}
+                  className="pr-10"
+                  onClick={() => ProcessAnimationBar("0")}
+                  onSelect={() => ProcessAnimationBar("0")}
+                >
                   <p id="0">home</p>
                 </Linc>
-                <Linc 
-                      to="blog"
-                      spy={true}
-                      smooth={true}
-                      offset={offset}
-                      duration={1000}
-                      isDynamic={true}
-                      ignoreCancelEvents={true}
-                      className="pr-10" onClick={() => ProcessAnimationBar("1")}
-                        onSelect={()=>ProcessAnimationBar('1')}>
+                <Linc
+                  to="blog"
+                  spy={true}
+                  smooth={true}
+                  offset={offset}
+                  duration={1000}
+                  isDynamic={true}
+                  ignoreCancelEvents={true}
+                  className="pr-10"
+                  onClick={() => ProcessAnimationBar("1")}
+                  onSelect={() => ProcessAnimationBar("1")}
+                >
                   <p id="1">blog</p>
                 </Linc>
                 <Linc
-                      to="guestbook"
-                      spy={true}
-                      smooth={true}
-                      offset={offset}
-                      isDynamic={true}
-                      ignoreCancelEvents={true}
-                       className="pr-10" onClick={() => ProcessAnimationBar("2")}
-                       onSelect={()=>ProcessAnimationBar('2')}>
+                  to="guestbook"
+                  spy={true}
+                  smooth={true}
+                  offset={offset}
+                  isDynamic={true}
+                  ignoreCancelEvents={true}
+                  className="pr-10"
+                  onClick={() => ProcessAnimationBar("2")}
+                  onSelect={() => ProcessAnimationBar("2")}
+                >
                   <p id="2">guestbook</p>
                 </Linc>
-                <Linc 
-                      to="resumé"
-                      spy={true}
-                      smooth={true}
-                      offset={offset}
-                      duration={1000}
-                      isDynamic={true}
-                      ignoreCancelEvents={true}
-                      className="pr-10" onClick={() => ProcessAnimationBar("3")}
-                      onSelect={()=>ProcessAnimationBar('3')}>
+                <Linc
+                  to="resumé"
+                  spy={true}
+                  smooth={true}
+                  offset={offset}
+                  duration={1000}
+                  isDynamic={true}
+                  ignoreCancelEvents={true}
+                  className="pr-10"
+                  onClick={() => ProcessAnimationBar("3")}
+                  onSelect={() => ProcessAnimationBar("3")}
+                >
                   <p id="3">resumé</p>
                 </Linc>
                 <Linc
-                      to="work"
-                      spy={true}
-                      smooth={true}
-                      offset={offset}
-                      duration={1000}
-                      isDynamic={true}
-                      ignoreCancelEvents={true}
-
+                  to="work"
+                  spy={true}
+                  smooth={true}
+                  offset={offset}
+                  duration={1000}
+                  isDynamic={true}
+                  ignoreCancelEvents={true}
                   className="w-full text-right"
                   onClick={() => ProcessAnimationBar("4")}
-                  onSelect={()=>ProcessAnimationBar('4')}
+                  onSelect={() => ProcessAnimationBar("4")}
                 >
                   <p id="4">work with me</p>
                 </Linc>
@@ -210,8 +209,10 @@ export default function Page() {
             let temp = elements;
             temp[0] = el.getBoundingClientRect().top;
             setElements(temp);
+            console.log(elements.toString())
+            if(elements[0] < 500 && elements[1] >= 500){ProcessAnimationBar('0')}
           }}
-          id='home'
+          id="home"
         >
           home
         </div>
@@ -222,8 +223,9 @@ export default function Page() {
             let temp = elements;
             temp[1] = el.getBoundingClientRect().top;
             setElements(temp);
+            if(elements[1] < 500 && elements[2] >= 500){ProcessAnimationBar('1')}
           }}
-          id='blog'
+          id="blog"
         >
           blog
         </div>
@@ -234,8 +236,9 @@ export default function Page() {
             let temp = elements;
             temp[2] = el.getBoundingClientRect().top;
             setElements(temp);
+            if(elements[2] < 500 && elements[3] >= 500){ProcessAnimationBar('2')}
           }}
-          id='guestbook'
+          id="guestbook"
         >
           guestbook
         </div>
@@ -246,8 +249,9 @@ export default function Page() {
             let temp = elements;
             temp[3] = el.getBoundingClientRect().top;
             setElements(temp);
+            if(elements[3] < 500 && elements[4] >= 500){ProcessAnimationBar('3')}
           }}
-          id='resumé'
+          id="resumé"
         >
           resumé
         </div>
@@ -258,8 +262,9 @@ export default function Page() {
             let temp = elements;
             temp[4] = el.getBoundingClientRect().top;
             setElements(temp);
+            if(elements[4] < 500 ){ProcessAnimationBar('4')}
           }}
-          id='work'
+          id="work"
         >
           work with me
         </div>
