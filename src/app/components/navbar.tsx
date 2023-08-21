4ea8a6cb969719bea0a047a423b69968920dcbd0
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-08-20 22:17:36 -0400

    now is better hehe

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -20,16 +20,16 @@ export default function Navbar(){
 
 
         for(let i = 0; i <= Number(toTab) -1; i++){before += document.getElementById(i.toString())?.offsetWidth!
-            console.log(before)}
+            before+=40}
 
         
 
         if(selectedTab < Number(toTab)){
         for(let i = selectedTab; i <= Number(toTab); i++){between += document.getElementById(i.toString())?.offsetWidth!
-        console.log(between)}
+        between += 40}
         }else{
         for(let i = selectedTab; i >= Number(toTab); i--){between += document.getElementById(i.toString())?.offsetWidth!
-        console.log(between)}
+        between += 40}
         }
 
         
@@ -57,10 +57,10 @@ export default function Navbar(){
         <nav>
             <div className="flex flex-col font-mono text-[16px] w-min">
                 <ul className="flex flex-row items-baseline">
-                    <li className="pr-10" id='0' onClick={()=>ProcessAnimationBar("0")} >home</li>
-                    <li className="pr-10" id='1' onClick={()=>ProcessAnimationBar("1")}>blog</li>
-                    <li className="pr-10" id='2' onClick={()=>ProcessAnimationBar("2")}>guestbook</li>
-                    <li className="pr-10" id='3' onClick={()=>ProcessAnimationBar("3")}>contact</li>
+                    <li className="mr-10" id='0' onClick={()=>ProcessAnimationBar("0")}>home</li>
+                    <li className="mr-10" id='1' onClick={()=>ProcessAnimationBar("1")}>blog</li>
+                    <li className="mr-10" id='2' onClick={()=>ProcessAnimationBar("2")}>guestbook</li>
+                    <li className="mr-10" id='3' onClick={()=>ProcessAnimationBar("3")}>contact</li>
                 </ul>
                 <div className="w-full bg-gray-200 h-[1px]   mt-2"></div>
                 <div style={{width: barWidth + 'px', transform: `translate(${barTranslate}px, -2px)`}} className={`bg-black transition-all duration-700  h-[3px]`}></div>
